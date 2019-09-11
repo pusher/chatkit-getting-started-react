79d526eea6d7fc79c9943d9519ff371383cdb0d6
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-09-11 20:06:26 +0100

    Remove imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-import { TokenProvider, ChatkitProvider } from '@pusher/chatkit-client-react';
-
 import './App.css';
 import Chat from './Chat';
 import UserList from './UserList';
 import Login from './Login';
 import chatkitLogo from './chatkit-logo.svg';
-
-const tokenProvider = new TokenProvider({
-  url:
-    'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/ff9d2a13-c35d-4b75-b791-9a5d05fde329/token',
-});
 
 function App() {
   const urlParams = new URLSearchParams(window.location.search);
